52188e238fd0e30b0d279450695fccad457b58e6
Muhahammad Iqbal <miqbaal1605@gmail.com>
2026-07-12 10:22:29 +0700

    [EDIT] Change name property in MachineModel from MaintenanceIntervalsDays to MaintenanceIntervalDays

--- smart-smca/Models/MachineModels.cs
+++ smart-smca/Models/MachineModels.cs
@@ -9,7 +9,7 @@ public class MachineModel
     public string Manufacturer { get; set; } = string.Empty;
     public int CalibrationIntervalDays { get; set; }
     public decimal CalibrationUsageThreshold { get; set; }
-    public int MaintenanceIntervalsDays { get; set; }
+    public int MaintenanceIntervalDays { get; set; }
     public decimal MaintenanceUsageThreshold { get; set; }
     public int GracePeriodDays { get; set; }
     public string ToleranceSpec { get; set; } = string.Empty;
